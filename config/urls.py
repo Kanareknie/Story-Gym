6df3cd68f3f15a59d5bf4afe1062df1905857a1b
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-03-16 06:11:46 +0000

    Create urls.py for core app, add paths in urls.py, settings and views.py

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core import views as core_views
 
 urlpatterns = [
-    path('', core_views.home, name='home'),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     
 ]
