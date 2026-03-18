09a951ea365bb126449dc40181f5724218c51a75
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-03-18 20:18:13 +0000

    Add secret key

--- config/settings.py
+++ config/settings.py
@@ -24,13 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cm3#nmc^45iw!ex)hzewrrn90+hb3%s!b&^&t=2bsh3t2=7u4h'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
-
 
 # Application definition
 
