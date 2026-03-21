7ef7931030bddf42aaf4cf22d834bb488dae4b97
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-03-21 19:21:48 +0000

    Add paths to urls.py core for login, logout and register

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import CustomLoginView, CustomLogoutView, register_view
 
 urlpatterns = [
     path('', views.home, name='home'), 
-    
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register_view, name='register'),
 ]
