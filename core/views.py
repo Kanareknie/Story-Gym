bc089e4b890ee916250dd54d6082e35c1c6ba61c
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-03-21 19:19:01 +0000

    Create views for login and register in views.py in core folder

--- core/views.py
+++ core/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import SignUpForm, LoginForm
+
 
 # Create your views here.
 
+# Home page view
 def home(request):
     return render(request, 'core/index.html')
+
+# Sign up view
+def register_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'core/register.html', {'form': form})
+
+# Custom login view using our LoginForm
+class CustomLoginView(LoginView):
+    template_name = 'core/login.html'
+    authentication_form = LoginForm
+
+# Custom logout view (can be extended if needed)
+class CustomLogoutView(LogoutView):
+    pass
