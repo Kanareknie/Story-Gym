1ea3c5ec60dbc8d3e904bc10e90267117a17f709
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-03-21 11:08:26 +0000

    Add model to admin.py to register it

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile, RandomizerResult, Story, Comment
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(RandomizerResult)
+admin.site.register(Story)
+admin.site.register(Comment)
+
