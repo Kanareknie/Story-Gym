9c69f26bb4da0f4b480b57163e4bbf08c7acf5e6
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-03-27 19:07:09 +0000

    Add email validation to register form

--- core/forms.py
+++ core/forms.py
@@ -5,11 +5,13 @@ from django.contrib.auth.hashers import make_password
 from .models import Profile
 import re
 
+
 class SignUpForm(UserCreationForm):
     # User table - add email
     email = forms.EmailField(required=True)
     # Profile table - add dob, security question and answer - only to reset the password purpuse
-    dob = forms.DateField(required=True, widget=forms.DateInput(attrs={'type': 'date'}))
+    dob = forms.DateField(
+        required=True, widget=forms.DateInput(attrs={'type': 'date'}))
     security_question = forms.ChoiceField(
         required=True,
         choices=[
@@ -22,28 +24,47 @@ class SignUpForm(UserCreationForm):
         required=True,
         widget=forms.TextInput(attrs={'autocomplete': 'off'})
     )
-    
+
     # Username validation
-    
+
     def clean_username(self):
         username = self.cleaned_data["username"]
     # Successfull username is at least 3 characters long
         if len(username) < 3:
-            raise forms.ValidationError("Username must be at least 3 characters long.")
+            raise forms.ValidationError(
+                "Username must be at least 3 characters long.")
     # Username can contains only letters, numbers and underscored
         if not re.match(r"^[A-Za-z0-9_]+$", username):
-            raise forms.ValidationError("Username can only contain letters, numbers, and underscores.")
+            raise forms.ValidationError(
+                "Username can only contain letters, numbers, and underscores.")
     # check if user already exists
         if User.objects.filter(username=username).exists():
             raise forms.ValidationError("This username is already taken.")
         return username
-    
-    
-    
+
+    # Email validation
+    # https://medium.com/@python-javascript-php-html-css/implementing-email-validation-in-django-projects-e210d4777fac
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        
+        # Block disposable domains
+        blocked_domains = ["tempmail.com", "mailinator.com", "10minutemail.com"]
+        domain = email.split("@")[-1]
+        if domain in blocked_domains:
+                raise forms.ValidationError(
+                    "Disposable email addresses are not allowed.")
+                
+        # Check for duplication of emails
+        if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("This email is already registered.")
+
+        return email
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2','dob','security_question','security_answer')
-    
+        fields = ('username', 'email', 'password1', 'password2',
+                  'dob', 'security_question', 'security_answer')
+
     # Creates the related profile immediately after the user is saved
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -59,8 +80,10 @@ class SignUpForm(UserCreationForm):
                 )
             )
         return user
-    
-    
+
+
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'autofocus': True}))
-    password = forms.CharField(label="Password", strip=False, widget=forms.PasswordInput)
+    username = forms.CharField(
+        max_length=150, widget=forms.TextInput(attrs={'autofocus': True}))
+    password = forms.CharField(
+        label="Password", strip=False, widget=forms.PasswordInput)
