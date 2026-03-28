cf6a3ab4e019de15c18218c89a74fd3fd0f88aa8
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-03-28 20:29:21 +0000

    Add path for write_now_view to urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import CustomLoginView, CustomLogoutView, register_view, randomizer_view, my_story_view, preview_story_view, repo_view, account_view
+from .views import CustomLoginView, CustomLogoutView, register_view, randomizer_view, write_now_view, my_story_view, preview_story_view, repo_view, account_view
 
 urlpatterns = [
     path('', views.home, name='home'), 
@@ -8,9 +8,11 @@ urlpatterns = [
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', register_view, name='register'),
     path('randomizer/', views.randomizer_view, name='randomizer'),
+    path('write_now/', views.write_now_view, name='write_now'),
     path('my_story/', views.my_story_view, name='my_story'),
     path('preview_story/', views.preview_story_view, name='preview_story'),
     path('repo/', views.repo_view, name='repo_story'),
     path('account/', views.account_view, name='account'),
+
     
 ]
