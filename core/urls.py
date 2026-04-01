b71aedfa072bf51f36760eb19143d0a0e0477285
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-04-01 18:19:53 +0100

    Debug 11 - Add edit and delete urls to urls.py to connect views with template account.html

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import CustomLoginView, CustomLogoutView, register_view, randomizer_view, write_now_view, my_story_view, preview_story_view, repo_view, account_view
+from .views import CustomLoginView, CustomLogoutView, register_view, randomizer_view, write_now_view, my_story_view, preview_story_view, repo_view, account_view, edit_story_view, delete_story_view
 
 urlpatterns = [
     path('', views.home, name='home'), 
@@ -13,6 +13,8 @@ urlpatterns = [
     path('preview_story/<int:story_id>/', views.preview_story_view, name='preview_story'),
     path('repo/', views.repo_view, name='repo_story'),
     path('account/', views.account_view, name='account'),
+    path('story/<int:story_id>/edit/', views.edit_story_view, name='edit_story'),
+    path('story/<int:story_id>/delete/', views.delete_story_view, name='delete_story'),
 
     
 ]
