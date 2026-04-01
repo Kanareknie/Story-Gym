f92431ff004bb41cec03a586d933432b520c6972
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-04-01 20:05:06 +0100

    Updated urls.py with delete_comment

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,25 @@
 from django.urls import path
 from . import views
-from .views import CustomLoginView, CustomLogoutView, register_view, randomizer_view, write_now_view, my_story_view, preview_story_view, repo_view, account_view, edit_story_view, delete_story_view, edit_comment_view
-
+from .views import CustomLoginView, CustomLogoutView, register_view, randomizer_view, write_now_view, my_story_view, preview_story_view, repo_view, account_view, edit_story_view, delete_story_view, edit_comment_view, delete_comment_view    
 urlpatterns = [
-    path('', views.home, name='home'), 
+    path('', views.home, name='home'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', register_view, name='register'),
     path('randomizer/', views.randomizer_view, name='randomizer'),
     path('write_now/', views.write_now_view, name='write_now'),
     path('my_story/', views.my_story_view, name='my_story'),
-    path('preview_story/<int:story_id>/', views.preview_story_view, name='preview_story'),
+    path('preview_story/<int:story_id>/',
+         views.preview_story_view, name='preview_story'),
     path('repo/', views.repo_view, name='repo_story'),
     path('account/', views.account_view, name='account'),
     path('story/<int:story_id>/edit/', views.edit_story_view, name='edit_story'),
-    path('story/<int:story_id>/delete/', views.delete_story_view, name='delete_story'),
-    path('comment/<int:comment_id>/edit/', views.edit_comment_view, name='edit_comment'),
+    path('story/<int:story_id>/delete/',
+         views.delete_story_view, name='delete_story'),
+    path('comment/<int:comment_id>/edit/',
+         views.edit_comment_view, name='edit_comment'),
+    path('comment/<int:comment_id>/delete/',
+         views.delete_comment_view, name='delete_comment'),
 
-    
+
 ]
