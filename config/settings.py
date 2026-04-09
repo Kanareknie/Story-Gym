c160335c7149e4777fbc3ed3ea082bfbce45433a
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-04-09 17:56:51 +0100

    Update links to articles in the index page

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# SECURItTY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
