041ce0e19f7a2cce47d517974e5e549f970da9e4
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-04-11 19:33:14 +0100

    Add Genre to admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Profile, RandomizerResult, Story, Comment
+from .models import Profile, RandomizerResult, Story, Comment, Genre
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(RandomizerResult)
 admin.site.register(Story)
 admin.site.register(Comment)
+admin.site.register(Genre)
 
